d21050f9c11248d5d15313cedc373e263e1a995e
jonny_g <jonathangormley23@gmail.com>
2023-01-27 13:37:31 +1100

    Added the global variables to js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const notRegistered = document.querySelector(".not-registered");
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const guessInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const spanGuesses = document.querySelector(".remaining span");
+const letterEntry = document.querySelector(".message");
+const playAgain = document.querySelector(".play-again");
+const word = "magnolia";
+
